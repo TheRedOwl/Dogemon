f6570e90659aa093470c0517c88c7456dd2cb6ff
TheRedOwl <v.adam3000@gmail.com>
2024-03-30 23:02:36 +0100

    Bit design and logic

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 import { fetchData } from "/fetchData.js";
 
-document.querySelector(".myBtn").addEventListener("click",listData)
+document.querySelector(".searchButton").addEventListener("click",listData)
 document.querySelector(".pagination").addEventListener("click",handlePagination)
 
 let page=1
-let pageSize=6
+let pageSize=5
 let totalPage=1
 let cars
 
 function listData(){
-    let marka=document.querySelector("input").value
-    const url=`https://api.api-ninjas.com/v1/cars?limit=50&make=${marka}`
+    let Breed=document.querySelector("input").value
+    const url=`https://api.api-ninjas.com/v1/dogs?name=${Breed}`
     fetchData(url,renderData)
 }
 
@@ -25,8 +25,45 @@ function showCars(){
     let startIndex=(page-1)*pageSize
     let endIndex=startIndex+pageSize
     let carsToShow=cars.slice(startIndex,endIndex)
+    let dogNumber = 1
     carsToShow.forEach(obj=>{
-        document.querySelector(".cars-list").innerHTML+=`<div class="myCard">${obj.model}</div>`
+        document.querySelector(".cars-list").innerHTML+=`
+        <div class="dogCard">
+            <div class="dogImg">
+                <img src="${obj.image_link}" class="" alt="">
+            </div>
+            <div class="dogName">${obj.name}</div>
+            <div class="stats">
+                <div>
+                    <div class="statName">Energy</div>
+                    <div class="statBar Energy${dogNumber}">
+                        
+                    </div>
+                </div>
+                <div>
+                    <div class="statName">Defense</div>
+                    <div class="statBar Defense${dogNumber}">
+                        
+                    </div>
+                </div>
+                <div>
+                    <div class="statName">Intelligence</div>
+                    <div class="statBar Intelligence${dogNumber}">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>`
+        for(let i=1;i<obj.energy;i++){
+            document.querySelector(`.Energy${dogNumber}`).innerHTML+=`<div class="statLevels${i}"></div>`
+        }
+        for(let i=1;i<obj.protectiveness;i++){
+            document.querySelector(`.Defense${dogNumber}`).innerHTML+=`<div class="statLevels${i}"></div>`
+        }
+        for(let i=1;i<obj.trainability;i++){
+            document.querySelector(`.Intelligence${dogNumber}`).innerHTML+=`<div class="statLevels${i}"></div>`
+        }
+        dogNumber++;
     })
     renderPagination(cars.length)
 }
