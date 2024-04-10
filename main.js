5a8d044c337ea62a1699cb59b0036f8ddba97c6a
Your Name <you@example.com>
2024-04-10 14:55:19 +0200

    login

--- main.js
+++ main.js
@@ -72,6 +72,7 @@ function showCars() {
         document.querySelector(".searchBar").placeholder = "Nincs találat"
     }
     document.querySelector(".pagination").classList.remove("hidden")
+    document.querySelector(".pagination").classList.add("flex")
     renderPagination(cars.length)
 }
 
@@ -167,4 +168,92 @@ function nextPage() {
 function lastPage() {
     page = totalPage
     showCars()
+}
+
+
+//login
+
+
+import { verifyAttr } from "./verifyAttr.js"
+
+document.querySelector(".loginBtn").addEventListener("click",auth)
+document.querySelector(".registerBtn").addEventListener("click",auth)
+
+function auth(e){
+    if(document.querySelector(".myInput").classList.contains("hidden")){
+        document.querySelectorAll(".myInput").forEach(obj=>{
+            obj.classList.remove("hidden")
+        })
+        return
+    }
+    console.log(e.target.textContent);
+    let username=document.getElementById("username").value
+    let pw=document.getElementById("pw").value
+    let users= JSON.parse(localStorage.getItem("users")) || []
+    //register esetén
+    if(e.target.textContent=="Register"){
+        if(username.length==0 ||pw.length==0) return
+        //nem lehet 2 egyforma fh név
+        if(verifyAttr(users,"username",username)){
+            document.querySelector("#message-container").innerHTML="Foglalt felhasználónév";
+            setTimeout(msgDelete, 3000)
+            return
+        }
+        users.push({username,pw})
+        localStorage.setItem("users",JSON.stringify(users))
+        document.querySelector("#message-container").innerHTML="Sikeres regisztráció, jelentkezz be!"
+        setTimeout(msgDelete, 3000)
+    }
+    else{//login esetén
+        let invalidUser=users.find(obj=>obj.username==username && obj.pw==pw)
+        if(invalidUser){
+            document.querySelector("#message-container").innerHTML="Sikeres bejelentkezés!"
+            setTimeout(msgDelete, 3000)
+            document.querySelector(".logoutBtn").title=username
+            document.querySelector(".searchBar").classList.remove("hidden")
+            document.querySelector(".searchButton").classList.remove("hidden")
+            localStorage.setItem("authUser",username)
+            verifyAuth()
+            hideInputs()
+        }else{
+            document.querySelector("#message-container").innerHTML="Hibás jelszó vagy felhasználónév!"
+            setTimeout(msgDelete, 3000)
+        }
+        
+    }
+}
+
+function verifyAuth(){
+    if(localStorage.getItem("authUser")){
+        console.log("be van jelentkezve", localStorage.getItem("authUser"));
+        document.querySelector(".loginBtn").classList.add("hidden")
+        document.querySelector(".registerBtn").classList.add("hidden")
+        document.querySelector(".logoutBtn").classList.remove("hidden")
+        document.querySelector(".logoutBtn").addEventListener("click",logoutUser)
+    } else{
+        console.log("Nincs felhasználó bejelentkezve");
+    }
+}
+verifyAuth()
+
+function logoutUser(){
+    localStorage.removeItem("authUser")
+    document.querySelector(".loginBtn").classList.remove("hidden")
+    document.querySelector(".registerBtn").classList.remove("hidden")
+    document.querySelector(".logoutBtn").classList.add("hidden")
+    document.querySelector(".searchBar").classList.add("hidden")
+    document.querySelector(".searchButton").classList.add("hidden")
+    document.querySelector("#message-container").innerHTML="Sikeresen kijelentkeztetve!"
+    setTimeout(msgDelete, 3000)
+    hideInputs()
+}
+
+function msgDelete(){
+    document.querySelector("#message-container").innerHTML=""
+}
+
+function hideInputs(){
+    document.querySelectorAll(".myInput").forEach(obj=>{
+        obj.classList.add("hidden")
+    })
 }
