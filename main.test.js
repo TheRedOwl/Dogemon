27e3d1e9f385c48373df83d738c4ef34f4802f35
The_Red_Owl <v.adam3000@gmail.com>
2024-04-24 14:22:15 +0200

    test

--- main.test.js
+++ main.test.js
@@ -6,12 +6,12 @@ describe("verifyPassword()",()=>{
         let pw = "asd123"
         expect(verifyPassword(pw)).toBe(true)
     })
-    test("A megadott jelszó helyesen 6-nál több karakter hosszú",()=>{
+    /*test("A megadott jelszó helyesen 6-nál több karakter hosszú",()=>{
         let pw = "asdf1234"
         expect(verifyPassword(pw)).toBe(true)
     })
     test("A megadott jelszó hibásan 6-nál kevesebb karakter hosszú",()=>{
         let pw = "asd1"
         expect(verifyPassword(pw)).toBe(false)
-    })
+    })*/
 })
